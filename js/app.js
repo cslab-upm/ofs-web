077c439c1cb0296c188aef2536ce7e1d79c3520e
RafaelTruji <rafaeltrujillo25@gmail.com>
2017-03-05 23:13:31 +0100

    Routing completo

--- js/app.js
+++ js/app.js
@@ -1,12 +1,40 @@
 var app = angular.module('observatorioapp', ['ngRoute']);
-
-app.config(function($routeProvider) {
+app.config(['$locationProvider', function($locationProvider) {
+  $locationProvider.hashPrefix('');
+}]);
+app.config(function($routeProvider, $locationProvider) {
     $routeProvider
-        .when('/inicio', {
+        .when('/', {
             templateUrl: 'templates/register.html',
             controller: ''
         })
+        .when('/inicio', {
+            templateUrl: 'templates/inicio.html',
+            controller: ''
+        })
+        .when('/experimento/nocturno', {
+            templateUrl: 'templates/night-exp.html',
+            controller: ''
+        })
+        .when('/experimento/solar', {
+            templateUrl: 'templates/solar-exp.html',
+            controller: ''
+        })
+        .when('/reservas', {
+          templateUrl: 'templates/reservations.html',
+          controller: ''
+        })
+        .when('/galeria', {
+          templateUrl: 'templates/gallery.html',
+          controller: ''
+        })
+        .when('/perfil', {
+          templateUrl: 'templates/profile.html',
+          controller: ''
+        })
         .otherwise({
-            redirectTo: '/inicio'
+            redirectTo: '/bienvenido'
         });
+
+        // $locationProvider.html5Mode(true);
 })
