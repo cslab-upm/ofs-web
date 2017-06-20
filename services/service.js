5e14d6a59ebb09181e5ccc7b325f6d22c7b49a3f
RafaelTruji <rafaeltrujillo25@gmail.com>
2017-06-20 20:22:14 +0200

    Added services.js

--- services/service.js
+++ services/service.js
@@ -17,13 +17,13 @@ app.factory("authFactory", function($cookies, $location, $rootScope, userFactory
             //     //mandamos a la home
             //     $location.path("/inicio");
             // }
-            $location.path("/inicio");
+            $location.path("/perfil");
         },
         register: function(name) {
           var expires = new Date();
           expires.setMinutes(expires.getMinutes() + 59);//cookie expira en 59 min
           $cookies.put('name', name, {'expires' : expires});
-          
+
         },
         toLogout: function() {
             $cookies.remove('name');
@@ -84,6 +84,10 @@ app.run(function($rootScope, authFactory) {
         authFactory.checkStatus();
     })
 });
+
+// app.run(function($http) {
+//   $http.defaults.headers.common.Authorization = 'Basic YWRtaW5pc3RyYXRvcjoxMjM0NTY3OA==';
+// });
 
 //Factoria operaciones GET estacion meteorologica
 // app.factory('weatherStationFactory', function($http) {
@@ -171,4 +175,133 @@ app.factory('userFactory', function() {
     }
 
     return user;
+});
+//Usuario
+app.factory('reservaFactory', function() {
+    //  var user = is logged username rol
+    var reserva = {};
+    reserva.experimento = null;//solar-nocturno
+    reserva.usuario = null;
+    reserva.inicio = null;
+    reserva.fin = null;
+    reserva.dia = null;
+
+    reserva.setExperimento = function(experimento){
+      this.experimento = experimento;
+    }
+    reserva.getExperimento = function(){
+      return this.experimento;
+    }
+    reserva.setUsuario = function(usuario){
+      this.usuario = usuario;
+    }
+    reserva.getUsuario = function(){
+      return this.usuario;
+    }
+    reserva.setInicio = function(inicio){
+      this.inicio = inicio;
+    }
+    reserva.getInicio = function(){
+      return this.inicio;
+    }
+    reserva.setFin = function(fin){
+      this.fin = fin;
+    }
+    reserva.getFin = function(){
+      return this.fin;
+    }
+    reserva.setDia = function(dia){
+      this.dia = dia;
+    }
+    reserva.getDia = function(){
+      return this.dia;
+    }
+
+    return recerva;
+});
+
+
+app.factory('Base64', function() {
+    var keyStr = 'ABCDEFGHIJKLMNOP' +
+            'QRSTUVWXYZabcdef' +
+            'ghijklmnopqrstuv' +
+            'wxyz0123456789+/' +
+            '=';
+    return {
+        encode: function (input) {
+            var output = "";
+            var chr1, chr2, chr3 = "";
+            var enc1, enc2, enc3, enc4 = "";
+            var i = 0;
+
+            do {
+                chr1 = input.charCodeAt(i++);
+                chr2 = input.charCodeAt(i++);
+                chr3 = input.charCodeAt(i++);
+
+                enc1 = chr1 >> 2;
+                enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+                enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+                enc4 = chr3 & 63;
+
+                if (isNaN(chr2)) {
+                    enc3 = enc4 = 64;
+                } else if (isNaN(chr3)) {
+                    enc4 = 64;
+                }
+
+                output = output +
+                        keyStr.charAt(enc1) +
+                        keyStr.charAt(enc2) +
+                        keyStr.charAt(enc3) +
+                        keyStr.charAt(enc4);
+                chr1 = chr2 = chr3 = "";
+                enc1 = enc2 = enc3 = enc4 = "";
+            } while (i < input.length);
+
+            return output;
+        },
+
+        decode: function (input) {
+            var output = "";
+            var chr1, chr2, chr3 = "";
+            var enc1, enc2, enc3, enc4 = "";
+            var i = 0;
+
+            // remove all characters that are not A-Z, a-z, 0-9, +, /, or =
+            var base64test = /[^A-Za-z0-9\+\/\=]/g;
+            if (base64test.exec(input)) {
+                alert("There were invalid base64 characters in the input text.\n" +
+                        "Valid base64 characters are A-Z, a-z, 0-9, '+', '/',and '='\n" +
+                        "Expect errors in decoding.");
+            }
+            input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+
+            do {
+                enc1 = keyStr.indexOf(input.charAt(i++));
+                enc2 = keyStr.indexOf(input.charAt(i++));
+                enc3 = keyStr.indexOf(input.charAt(i++));
+                enc4 = keyStr.indexOf(input.charAt(i++));
+
+                chr1 = (enc1 << 2) | (enc2 >> 4);
+                chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+                chr3 = ((enc3 & 3) << 6) | enc4;
+
+                output = output + String.fromCharCode(chr1);
+
+                if (enc3 != 64) {
+                    output = output + String.fromCharCode(chr2);
+                }
+                if (enc4 != 64) {
+                    output = output + String.fromCharCode(chr3);
+                }
+
+                chr1 = chr2 = chr3 = "";
+                enc1 = enc2 = enc3 = enc4 = "";
+
+            } while (i < input.length);
+
+            return output;
+        }
+    };
 });
