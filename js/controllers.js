d3c85b600fbdd49452d729b6baaf99288d5b5808
alvarogtx300 <alvarogtx300@gmail.com>
2018-06-22 22:14:28 +0200

    Integración con registros de usuarios

--- js/controllers.js
+++ js/controllers.js
@@ -221,76 +221,67 @@ app.controller('contactoController', ['$scope', '$sce', '$http', function($scope
 
 }]);
 
-app.controller('registrarController', ['$rootScope','$scope', '$sce', '$http', 'authFactory', 'userFactory', function($rootScope, $scope, $sce, $http, authFactory, userFactory) {
-    // <!-- Google Analytics -->
-    ga('set', 'page', '/registrar');
-    ga('send', 'pageview');
-    // <!-- End Google Analytics -->
+app.controller('registrarController', ['$rootScope', '$scope', '$sce', '$http', 'authFactory', 'userFactory', function ($rootScope, $scope, $sce, $http, authFactory, userFactory) {
+	$('.nav').find('.active').removeClass('active');
+	$('#form-login').hide();
+	$('#form-recoveryPassword').hide();
+	$('#h2_log').hide();
+	$('#h2_repass').hide();
 
-    $('.nav').find('.active').removeClass('active');
-    $('#form-login').hide();
-    $('#form-recoveryPassword').hide();
-    $('#h2_log').hide();
-    $('#h2_repass').hide();
+	// $rootScope.user = {};
+	// $rootScope.isLogged = userFactory.getIsLogged();
 
-    // $rootScope.user = {};
-    // $rootScope.isLogged = userFactory.getIsLogged();
+	$scope.user = {};
+	$scope.user.nameRegister = null;
+	$scope.user.emailRegister = null;
+	$scope.user.passwordRegister1 = null;
+	$scope.user.passwordRegister2 = null;
+	$scope.user.isLogged = false;
 
-    $scope.user = {};
-    $scope.user.nameRegister = null;
-    $scope.user.emailRegister = null;
-    $scope.user.passwordRegister1 = null;
-    $scope.user.passwordRegister2 = null;
-    $scope.user.isLogged = false;
+	$scope.register = {};
+	$scope.register.errorName = false;
+	$scope.register.errorEmail = false;
+	$scope.register.errorPassword = false;
+	$scope.register.errorEnvio = false;
 
-    $scope.register = {};
-    $scope.register.errorName = false;
-    $scope.register.errorEmail = false;
-    $scope.register.errorPassword = false;
-    $scope.register.errorEnvio = false;
+	$scope.toRegister = function () {
+		//validar entradas
+		if ($scope.user.passwordRegister1 !== $scope.user.passwordRegister2) {
+			$scope.register.errorPassword = true;
 
-    $scope.toRegister = function(){
-      //validar entradas
-      if( $scope.user.passwordRegister1 != $scope.user.passwordRegister2){
-        $scope.register.errorPassword = true;
-
-      }else {
-        $scope.register.errorPassword = false;
-        userFactory.setName($scope.user.nameRegister);
-        userFactory.setEmail($scope.user.emailRegister);
-        userFactory.setPassword($scope.user.passwordRegister1);
-        $('#form-submit-reg').hide();
-        $('#register-gif').show();
+		} else {
+			$scope.register.errorPassword = false;
+			userFactory.setName($scope.user.nameRegister);
+			userFactory.setEmail($scope.user.emailRegister);
+			userFactory.setPassword($scope.user.passwordRegister1);
+			$('#form-submit-reg').hide();
+			$('#register-gif').show();
 
 
-        $http({
-          url: 'http://localhost:8080/things/gatekeeper/registerUser',
-          method: 'POST',
-          data : {name: $scope.user.nameRegister, email: $scope.user.emailRegister, password: $scope.user.passwordRegister1},
-          headers : {'Content-Type': 'application/x-www-form-urlencoded'}
-        }).then(function successCallback(response) {
-          if (response.status == 201) {
-            // userFactory.setIsLogged(true);
-            // $rootScope.isLogged = userFactory.getIsLogged();
+			$http({
+				url: 'http://localhost:8080/register',
+				method: 'POST',
+				data: {
+					username: $scope.user.nameRegister,
+					email: $scope.user.emailRegister,
+					password: $scope.user.passwordRegister1
+				},
+			}).then(function successCallback(response) {
+				if (response.status === 201) {
+					// Si registro correcto, mostar mensaje
+					$('#login-gif').hide();
+					$('#form-register').hide();
+					$('#confirmEmail').show();
+				}
+				// return response;
+			}, function errorCallback(response) {
+				$('#register-gif').hide();
+				$('#form-submit-reg').show();
+				$scope.register.errorEnvio = true;
 
-            authFactory.register($scope.user.nameRegister);//cookie
-            // $scope.user.isLogged = true;
-            $('#login-gif').hide();
-            $('#form-register').hide();
-            $('#confirmEmail').show();
-          }
-          // return response;
-        }, function errorCallback(response) {
-          $('#register-gif').hide();
-          $('#form-submit-reg').show();
-          $scope.register.errorEnvio = true;
-
-        });
-
-      }
-
-    }
-
+			});
+		}
+	}
 }]);
 
 app.controller('loginController', ['$rootScope', '$scope', '$sce', '$http', '$location', 'authFactory','userFactory','Base64','httpFactory', function($rootScope, $scope, $sce, $http, $location, authFactory,userFactory, Base64, httpFactory) {
